cbb59c1ddb2e3ef30eca583e42a8a0ca1b62ad0c
Joseph Young <musicjoeyoung@gmail.com>
2023-10-26 16:07:49 -0400

    peoplepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import HomePage from "./pages/HomePage/HomePage";
+import Header from "./components/Header/Header";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import NotFound from "./pages/NotFound/NotFound";
+import PeoplePage from "./pages/PeoplePage/PeoplePage";
 
 function App() {
   return (
-    <h1>
-      <HomePage />
-    </h1>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/people" element={<PeoplePage />} />
+        <Route path="/people/:peopleId" element={<PeoplePage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
